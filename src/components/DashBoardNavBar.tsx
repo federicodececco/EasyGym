8d74c4b18cfce901e219deb185ac2977a13c05f2
federicodececco <federicodececco1@gmail.com>
2025-09-30 12:00:09 +0200

    feat: added active status to navbar buttons

--- src/components/DashBoardNavBar.tsx
+++ src/components/DashBoardNavBar.tsx
@@ -1,18 +1,44 @@
+"use client";
 import { Dumbbell, LogOut, User } from "lucide-react";
+import { useState } from "react";
 
 export default function DashBoardNavBar() {
+  const [activeIndex, setActiveIndex] = useState(0);
   return (
     <>
-      <nav className=" h-full w-16 flex flex-col justify-center bg-gradient-to-br from-slate-700 to-slate-800 border-slate-900 border-r-2 ">
-        <div className=" flex flex-col align-middle ">
-          <button className=" group  group-hover:opacity-100 duration-300 group-hover:scale-[1.02] hover:bg-slate-500 py-10">
-            <Dumbbell className="group-hover:scale-[1.5] duration-300 mx-auto " />
+      <nav className="h-full w-16 flex flex-col justify-center bg-gradient-to-br from-slate-700 to-slate-800 border-slate-900 border-r-2">
+        <div className="flex flex-col align-middle">
+          <button
+            onClick={() => setActiveIndex(0)}
+            className={`group duration-300 group-hover:scale-[1.02] py-10 ${
+              activeIndex === 0
+                ? "bg-slate-500 border-l-4 border-blue-500"
+                : "hover:bg-slate-500"
+            }`}
+          >
+            <Dumbbell className="group-hover:scale-[1.5] duration-300 mx-auto" />
           </button>
-          <button className=" group  group-hover:opacity-100 duration-300 group-hover:scale-[1.02] hover:bg-slate-500 py-10">
-            <User className="group-hover:scale-[1.5] duration-300 mx-auto " />
+
+          <button
+            onClick={() => setActiveIndex(1)}
+            className={`group duration-300 group-hover:scale-[1.02] py-10 ${
+              activeIndex === 1
+                ? "bg-slate-500 border-l-4 border-blue-500"
+                : "hover:bg-slate-500"
+            }`}
+          >
+            <User className="group-hover:scale-[1.5] duration-300 mx-auto" />
           </button>
-          <button className=" group  group-hover:opacity-100 duration-300 group-hover:scale-[1.02] hover:bg-slate-500 py-10">
-            <LogOut className="group-hover:scale-[1.5] duration-300 mx-auto " />
+
+          <button
+            onClick={() => setActiveIndex(2)}
+            className={`group duration-300 group-hover:scale-[1.02] py-10 ${
+              activeIndex === 2
+                ? "bg-slate-500 border-l-4 border-blue-500"
+                : "hover:bg-slate-500"
+            }`}
+          >
+            <LogOut className="group-hover:scale-[1.5] duration-300 mx-auto" />
           </button>
         </div>
       </nav>
