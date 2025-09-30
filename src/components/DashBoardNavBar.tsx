5b18ff179d003b2eaa55d45db0d29455d4832bd0
federicodececco <federicodececco1@gmail.com>
2025-09-30 12:22:34 +0200

    feat:added navigation to navbar

--- src/components/DashBoardNavBar.tsx
+++ src/components/DashBoardNavBar.tsx
@@ -1,16 +1,23 @@
 "use client";
 import { Dumbbell, LogOut, User } from "lucide-react";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function DashBoardNavBar() {
   const [activeIndex, setActiveIndex] = useState(0);
+  const router = useRouter();
+  const handleNavigation = (index: number, path: string) => {
+    setActiveIndex(index);
+    router.replace(path);
+  };
+
   return (
     <>
       <nav className="h-full w-16 flex flex-col justify-center bg-gradient-to-br from-slate-700 to-slate-800 border-slate-900 border-r-2">
         <div className="flex flex-col align-middle">
           <button
-            onClick={() => setActiveIndex(0)}
-            className={`group duration-300 group-hover:scale-[1.02] py-10 ${
+            onClick={() => handleNavigation(0, "/")}
+            className={`group duration-300 py-10 ${
               activeIndex === 0
                 ? "bg-slate-500 border-l-4 border-blue-500"
                 : "hover:bg-slate-500"
@@ -20,8 +27,8 @@ export default function DashBoardNavBar() {
           </button>
 
           <button
-            onClick={() => setActiveIndex(1)}
-            className={`group duration-300 group-hover:scale-[1.02] py-10 ${
+            onClick={() => handleNavigation(1, "/user")}
+            className={`group duration-300 py-10 ${
               activeIndex === 1
                 ? "bg-slate-500 border-l-4 border-blue-500"
                 : "hover:bg-slate-500"
